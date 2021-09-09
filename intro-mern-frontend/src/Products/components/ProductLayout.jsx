b93fab918011051e5ffff470ec7a4393bfef2760
FrancoMostafa <francom2342@gmail.com>
2021-09-09 19:49:46 -0300

    clase 4{

--- intro-mern-frontend/src/Products/components/ProductLayout.jsx
+++ intro-mern-frontend/src/Products/components/ProductLayout.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./Header";
 import AddButton from "./AddButton";
-import Loading from "./Loading";
-import { Button } from "react-bulma-components";
+import ListProduct from "./ListProducts";
 
 const ProductLayout = () => {
-  const [isLoading, setIsLoading] = useState(false);
   return (
     <>
       <Header title="Products App"></Header>
       <AddButton />
-      {isLoading ? <Loading /> : "mostrar resultado fetch"}
+      <ListProduct />
     </>
   );
 };
